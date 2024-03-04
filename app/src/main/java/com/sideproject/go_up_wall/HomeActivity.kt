cd07bad728e29a0bdbcca33343e93ac93f8d47da
kimchungdo <dorian21@naver.com>
2024-03-05 00:26:39 +0900

    init status

---
+++ app/src/main/java/com/sideproject/go_up_wall/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.sideproject.go_up_wall
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "go_up_wall"
+include(":app")
+ 
