0391b8b9aef1dc5c8ea86f974f5bf2f24976cb16
kimchungdo <dorian21@naver.com>
2024-03-06 00:03:46 +0900

    fix install issue, add setting activity

---
+++ app/src/main/java/com/sideproject/go_up_wall/SettingActivity.kt
@@ -0,0 +1,11 @@
+package com.sideproject.go_up_wall
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
