f27b5acbb080b5f9cfd779f0ff8c6c0cf734dd83
kimchungdo <dorian21@naver.com>
2024-03-25 21:56:36 +0900

    add marquee

--- app/src/main/java/com/sideproject/go_up_wall/HomeActivity.kt
+++ app/src/main/java/com/sideproject/go_up_wall/HomeActivity.kt
@@ -2,10 +2,13 @@ package com.sideproject.go_up_wall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val marqueeButton = findViewById<AppCompatButton>(R.id.marquee_button)
+        marqueeButton.isSelected = true
     }
 }
